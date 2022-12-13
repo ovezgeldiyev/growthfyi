dbb9dee89de0e4e42337471f2afc955f7f0d95d9
ovezgeldiyev <ovezgeldiyevagabay2000@gmail.com>
2022-12-13 20:43:00 +0500

    asd

--- js/main.js
+++ js/main.js
@@ -1,60 +1,33 @@
-// // menu start
-// var menu = document.getElementById("menu");
-// var menuBtn = document.getElementById("menuBtn");
-// var body = document.body;
-// menuBtn.onclick = function () {
-//   menu.classList.toggle("active");
-//   menuBtn.classList.toggle("active");
-//   body.classList.toggle("active");
-// };
-// window.onclick = function (event) {
-//   if (event.target == menu) {
-//     menu.classList.remove("active");
-//     menuBtn.classList.remove("active");
-//     body.classList.remove("active");
-//   }
-// };
-// // menu end
+// menu start
+var menu = document.getElementById("menu");
+var menuBtn = document.getElementById("menuBtn");
+var body = document.body;
+menuBtn.onclick = function () {
+  menu.classList.toggle("active");
+  menuBtn.classList.toggle("active");
+};
+window.onclick = function (event) {
+  if (event.target == menu) {
+    menu.classList.remove("active");
+    menuBtn.classList.remove("active");
+  }
+};
+// menu end
 
-// // scroll start
-// let header = document.getElementById("header");
-// function scrollFunc() {
-//   if (window.pageYOffset >= 60) {
-//     header.classList.add("sticky");
-//   } else {
-//     header.classList.remove("sticky");
-//   }
-// }
-// const links = document.querySelectorAll(".links");
-// const sections = document.querySelectorAll(".anchor");
-// function changeLinkState() {
-//   let index = sections.length;
-//   while (--index && window.scrollY + 100 < sections[index].offsetTop) {}
-//   links.forEach((link) => link.classList.remove("active"));
-//   links[index]?.classList.add("active");
-// }
-// links.forEach((e) => {
-//   onLinkClick(e);
-// });
-// function onLinkClick(linkItem) {
-//   linkItem.addEventListener("click", function () {
-//     menu.classList.remove("active");
-//     menuBtn.classList.remove("active");
-//     body.classList.remove("active");
-//   });
-// }
-// function onLinkClick(linkItem) {
-//   linkItem.addEventListener("click", function () {
-//     menu.classList.remove("active");
-//     menuBtn.classList.remove("active");
-//     body.classList.remove("active");
-//   });
-// }
-// window.onscroll = function () {
-//   changeLinkState();
-//   scrollFunc();
-// };
-// // scroll end
+// scroll start
+let header = document.getElementById("header");
+function scrollFunc() {
+  if (window.pageYOffset >= 40) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+window.onscroll = function () {
+  scrollFunc();
+};
+// scroll end
 
 // faq start
 const tabBtn = document.querySelectorAll(".tabBtn");
