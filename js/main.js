62c8684b8d136b09bea79cd664cbecbbfc3f28e7
ovezgeldiyev <ovezgeldiyevagabay2000@gmail.com>
2022-12-14 19:18:46 +0500

    asdas

--- js/main.js
+++ js/main.js
@@ -6,12 +6,8 @@ menuBtn.onclick = function () {
   menu.classList.toggle("active");
   menuBtn.classList.toggle("active");
 };
-window.onclick = function (event) {
-  if (event.target == menu) {
-    menu.classList.remove("active");
-    menuBtn.classList.remove("active");
-  }
-};
+
+
 // menu end
 
 // scroll start
@@ -28,37 +24,5 @@ window.onscroll = function () {
   scrollFunc();
 };
 // scroll end
-
-// faq start
-const tabBtn = document.querySelectorAll(".tabBtn");
-const tabEvent = document.querySelectorAll(".tabEvent");
-tabBtn.forEach((e) => {
-  onTabClick(tabBtn, tabEvent, e);
-});
-function onTabClick(tabBtns, tabItems, item) {
-  item.addEventListener("click", function (e) {
-    let currentBtn = item;
-    let tabId = currentBtn.getAttribute("data-tab");
-    let currentTab = document.querySelector(tabId);
-    if (currentBtn.classList.contains("active")) {
-      const faq = currentBtn.parentElement.querySelector(".tabEvent");
-      if (faq) {
-        faq.classList.remove("active");
-        currentBtn.classList.remove("active");
-      }
-    } else if (!currentBtn.classList.contains("active")) {
-      tabBtns.forEach(function (item) {
-        item.classList.remove("active");
-      });
-
-      tabItems.forEach(function (item) {
-        item.classList.remove("active");
-      });
-      currentBtn.classList.add("active");
-      currentTab.classList.add("active");
-    }
-  });
-}
-// faq end
 
 
